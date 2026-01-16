7bd7bd5ace0e9887b5bc68e5008a66c635d21102
Aditya Singh <adityasinghdpsjkp.3597@gmail.com>
2026-01-16 16:30:46 +0530

    Time: 27 ms (73.67%) | Memory: 42.5 MB (17.43%) - LeetSync

--- LeetCode/234-palindrome-linked-list/palindrome-linked-list.py
+++ LeetCode/234-palindrome-linked-list/palindrome-linked-list.py
@@ -3,7 +3,6 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         slow, fast, prev = head, head, None
@@ -17,3 +16,4 @@ class Solution:
             if fast.val != slow.val: return False
             fast, slow = fast.next, slow.next
         return True
+        
